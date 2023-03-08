9962552efe224357e58e8e53754971869e10869c
francescoolivieri <francescoolivieri45@gmail.com>
2023-03-08 23:57:49 +0100

    Racing layout improvements & other

--- steering/ui/tab_manager.c
+++ steering/ui/tab_manager.c
@@ -10,6 +10,7 @@ void tab_manager(void)
     lv_obj_t *tab2 = lv_tabview_add_tab(tab_view, "Tab 2");
     lv_obj_t *tab3 = lv_tabview_add_tab(tab_view, "Tab 3");
 
+    lv_obj_clear_flag(tab1, LV_OBJ_FLAG_SCROLLABLE);
     tab_racing(tab1);
 
     lv_obj_t * label1 = lv_label_create(tab2);
--- steering/ui/tab_racing.c
+++ steering/ui/tab_racing.c
@@ -1,14 +1,7 @@
 #include "tab_racing.h"
 
-void tab_racing(lv_obj_t * parent)
-{
-    static lv_style_t grid_style;
-    lv_style_init(&grid_style);
-    lv_style_set_pad_all(&grid_style, 0);
-    lv_style_set_pad_bottom(&grid_style, 0);
-    lv_style_set_pad_column(&grid_style, 0);
-    lv_style_set_pad_top(&grid_style, 0);
-    lv_style_set_pad_row(&grid_style, 0);
+void tab_racing(lv_obj_t * parent){
+    init_custom_styles();
 
     static lv_coord_t main_panel_cols[] =  {SIDE_BAR_WIDTH, CENTRAL_PANEL_WIDTH, SIDE_BAR_WIDTH, LV_GRID_TEMPLATE_LAST};
     static lv_coord_t main_panel_rows[] = {SCREEN_HEIGHT, LV_GRID_TEMPLATE_LAST};
@@ -17,27 +10,17 @@ void tab_racing(lv_obj_t * parent)
     lv_obj_set_layout(tab_racing, LV_LAYOUT_GRID);
     lv_obj_clear_flag(tab_racing, LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_set_size(tab_racing, SCREEN_WIDTH, SCREEN_HEIGHT);
-
     lv_obj_add_style(tab_racing, &grid_style, 0);
     lv_obj_center(tab_racing);
-    lv_obj_set_style_base_dir(tab_racing, LV_BASE_DIR_RTL, 0);
+    
     lv_obj_set_grid_dsc_array(tab_racing, main_panel_cols, main_panel_rows);
-
-
-    static lv_style_t bar_style;
-
-    lv_style_init(&bar_style);
-    lv_style_set_bg_opa(&bar_style, LV_OPA_COVER);
-    lv_style_set_bg_color(&bar_style, lv_palette_main(LV_PALETTE_RED));
-    lv_style_set_bg_grad_color(&bar_style, lv_palette_main(LV_PALETTE_BLUE));
-    lv_style_set_bg_grad_dir(&bar_style, LV_GRAD_DIR_VER);
 
 
     // LEFT BAR
     lv_obj_t* hv_bar = lv_bar_create(tab_racing);
     lv_obj_remove_style_all(hv_bar);
     lv_obj_add_style(hv_bar, &bar_style, LV_PART_INDICATOR);
-    lv_obj_set_size(hv_bar, 20, 200);
+    lv_obj_set_size(hv_bar, 65, 300);
     lv_obj_center(hv_bar);
     lv_bar_set_range(hv_bar, -20, 40);
     lv_obj_set_grid_cell(hv_bar, LV_GRID_ALIGN_STRETCH, 2, 1,
@@ -60,10 +43,9 @@ void tab_racing(lv_obj_t * parent)
     lv_obj_set_grid_dsc_array(central_panel, cpanel_cols, cpanel_rows);
 
     // CHANGE-TABS VIEW (I don't remember the right name)
-    lv_obj_t * o1 = lv_obj_create(central_panel);
-    lv_obj_t * l1 = lv_label_create(o1);
+    lv_obj_t * l1 = lv_label_create(central_panel);
     lv_label_set_text(l1, "change-tabs view");
-    lv_obj_set_grid_cell(o1, LV_GRID_ALIGN_STRETCH, 0, 1,
+    lv_obj_set_grid_cell(l1, LV_GRID_ALIGN_STRETCH, 0, 1,
                          LV_GRID_ALIGN_STRETCH, 0, 1);
 
     // DATA PANEL
@@ -81,24 +63,40 @@ void tab_racing(lv_obj_t * parent)
     lv_obj_set_grid_dsc_array(data_panel, dpanel_cols, dpanel_rows);
 
     // DATA LEFT
-    lv_obj_t * o4 = lv_obj_create(data_panel);
-    lv_obj_t * l4 = lv_label_create(o4);
+    lv_obj_t * l4 = lv_label_create(data_panel);
     lv_label_set_text(l4, "data left");
-    lv_obj_set_grid_cell(o4, LV_GRID_ALIGN_STRETCH, 0, 1,
+    lv_obj_set_grid_cell(l4, LV_GRID_ALIGN_STRETCH, 0, 1,
                          LV_GRID_ALIGN_STRETCH, 0, 1);
 
     // DATA CENTER
-    lv_obj_t * o5 = lv_obj_create(data_panel);
-    lv_obj_t * l5 = lv_label_create(o5);
-    lv_label_set_text(l5, "data center");
-    lv_obj_set_grid_cell(o5, LV_GRID_ALIGN_STRETCH, 1, 1,
-                         LV_GRID_ALIGN_STRETCH, 0, 1);
+
+    // power
+    lv_obj_t * power = lv_label_create(data_panel);
+    lv_obj_add_style(power, &label_style, LV_PART_MAIN);
+    lv_obj_set_style_text_font(power, &lv_font_inter_bold_22, LV_STATE_DEFAULT);
+    lv_label_set_text(power, "POWER 60%");
+    lv_obj_set_grid_cell(power, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_START, 0, 1);
+
+    // speedometer
+    lv_obj_t *speed = lv_vertical_pair_label(data_panel, "169", &lv_font_inter_bold_70, "km/h", &lv_font_inter_bold_22);
+    lv_obj_set_grid_cell(speed, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+
+    lv_obj_t *custom_meter = lv_meter_create(lv_scr_act());
+    lv_custom_meter(custom_meter);
+    lv_obj_align(custom_meter, LV_ALIGN_CENTER, 0, 15);
+
+    // lap counter
+    lv_obj_t *lap_counter = lv_label_create(data_panel);
+    lv_obj_add_style(lap_counter, &label_style, LV_PART_MAIN);
+    lv_label_set_text(lap_counter, "  12\nLAP");
+    lv_obj_set_grid_cell(lap_counter, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_END, 0, 1);
+    lv_obj_set_style_text_font(lap_counter, &lv_font_inter_bold_22, LV_STATE_DEFAULT);
+    lv_obj_set_style_pad_bottom(lap_counter, 10, 0);
 
     // DATA RIGHT
-    lv_obj_t * o6 = lv_obj_create(data_panel);
-    lv_obj_t * l6 = lv_label_create(o6);
+    lv_obj_t * l6 = lv_label_create(data_panel);
     lv_label_set_text(l6, "data right");
-    lv_obj_set_grid_cell(o6, LV_GRID_ALIGN_STRETCH, 2, 1,
+    lv_obj_set_grid_cell(l6, LV_GRID_ALIGN_STRETCH, 2, 1,
                          LV_GRID_ALIGN_STRETCH, 0, 1);
 
 
@@ -107,10 +105,9 @@ void tab_racing(lv_obj_t * parent)
                          LV_GRID_ALIGN_STRETCH, 1, 1);
 
     // BOTTOM BAR
-    lv_obj_t * o3 = lv_obj_create(central_panel);
-    lv_obj_t * l3 = lv_label_create(o3);
+    lv_obj_t * l3 = lv_label_create(central_panel);
     lv_label_set_text(l3, "bottom bar");
-    lv_obj_set_grid_cell(o3, LV_GRID_ALIGN_STRETCH, 0, 1,
+    lv_obj_set_grid_cell(l3, LV_GRID_ALIGN_STRETCH, 0, 1,
                          LV_GRID_ALIGN_STRETCH, 2, 1);
 
     // setting central panel position in tab racing
@@ -122,7 +119,7 @@ void tab_racing(lv_obj_t * parent)
     lv_obj_t* lv_bar = lv_bar_create(tab_racing);
     lv_obj_remove_style_all(lv_bar);
     lv_obj_add_style(lv_bar, &bar_style, LV_PART_INDICATOR);
-    lv_obj_set_size(lv_bar, 20, 200);
+    lv_obj_set_size(lv_bar, 65, 300);
     lv_obj_center(lv_bar);
     lv_bar_set_range(lv_bar, -20, 40);
     lv_obj_set_grid_cell(lv_bar, LV_GRID_ALIGN_STRETCH, 0, 1,
