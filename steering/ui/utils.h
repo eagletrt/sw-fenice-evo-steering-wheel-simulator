401d14d93b6437ea49e9b1988835af0718ed1e28
francescoolivieri <francescoolivieri45@gmail.com>
2023-04-19 15:27:44 +0200

    new fonts at 2bpp

--- steering/ui/utils.h
+++ steering/ui/utils.h
@@ -25,35 +25,17 @@
 #define COLOR_RED_STATUS_HEX 0xCE0000
 
 
-LV_FONT_DECLARE(lv_font_inter_bold_10);
-LV_FONT_DECLARE(lv_font_inter_bold_12);
+
 LV_FONT_DECLARE(lv_font_inter_bold_14);
-LV_FONT_DECLARE(lv_font_inter_bold_15);
-LV_FONT_DECLARE(lv_font_inter_bold_16);
 LV_FONT_DECLARE(lv_font_inter_bold_18);
 LV_FONT_DECLARE(lv_font_inter_bold_20);
 LV_FONT_DECLARE(lv_font_inter_bold_22);
 LV_FONT_DECLARE(lv_font_inter_bold_24);
-LV_FONT_DECLARE(lv_font_inter_bold_26);
-LV_FONT_DECLARE(lv_font_inter_bold_28);
 LV_FONT_DECLARE(lv_font_inter_bold_30);
-LV_FONT_DECLARE(lv_font_inter_bold_32);
-LV_FONT_DECLARE(lv_font_inter_bold_34);
-LV_FONT_DECLARE(lv_font_inter_bold_36);
 LV_FONT_DECLARE(lv_font_inter_bold_38);
 LV_FONT_DECLARE(lv_font_inter_bold_40);
-LV_FONT_DECLARE(lv_font_inter_bold_42);
-LV_FONT_DECLARE(lv_font_inter_bold_44);
-LV_FONT_DECLARE(lv_font_inter_bold_46);
-LV_FONT_DECLARE(lv_font_inter_bold_48);
-LV_FONT_DECLARE(lv_font_inter_bold_50);
-LV_FONT_DECLARE(lv_font_inter_bold_52);
-LV_FONT_DECLARE(lv_font_inter_bold_54);
-LV_FONT_DECLARE(lv_font_inter_bold_56);
-LV_FONT_DECLARE(lv_font_inter_bold_58);
 LV_FONT_DECLARE(lv_font_inter_bold_60);
 LV_FONT_DECLARE(lv_font_inter_bold_70);
-LV_FONT_DECLARE(lv_font_inter_bold_80);
 
 extern lv_style_t grid_style;
 extern lv_style_t label_style;
