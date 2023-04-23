b405f3549b9463257eca58f48e5bc53ba463cd20
tzarjakob <giacomo.mazzucchi@protonmail.com>
2023-04-23 22:07:27 +0200

    refactor: macro for declare steering values

--- steering/steering.h
+++ steering/steering.h
@@ -6,129 +6,62 @@
 #include "lvgl/lvgl.h"
 #include <time.h>
 
+#define LV_PROPERTY(type, name) \
+    type name; \
+    lv_obj_t * lb_##name;
+
 typedef struct {
  
-/*----primary messages-----*/
-    uint8_t value_AMBIENT_TEMPERATURE;
-    lv_obj_t * lb_AMBIENT_TEMPERATURE;
-
+    /*----primary messages-----*/
+    LV_PROPERTY(uint8_t, AMBIENT_TEMPERATURE)
 
     /*--HV system--*/
-    uint8_t value_HV_VOLTAGE;
-    lv_obj_t * lb_HV_VOLTAGE;
-
-    uint8_t value_HV_CURRENT;
-    lv_obj_t * lb_HV_CURRENT;
-
-    uint8_t value_HV_TEMP;
-    lv_obj_t * lb_HV_TEMP;
-
-    uint8_t value_HV_PERCENT;
-    lv_obj_t * lb_HV_PERCENT;
-
+    LV_PROPERTY(uint8_t, HV_VOLTAGE)
+    LV_PROPERTY(uint8_t, HV_CURRENT)
+    LV_PROPERTY(uint8_t, HV_TEMP)
+    LV_PROPERTY(uint8_t, HV_PERCENT)
 
     /*--LV system--*/
-    uint8_t value_LV_VOLTAGE;
-    lv_obj_t * lb_LV_VOLTAGE;
+    LV_PROPERTY(uint8_t, LV_VOLTAGE)
+    LV_PROPERTY(uint8_t, LV_CURRENT)
+    LV_PROPERTY(uint8_t, LV_TEMP)
+    LV_PROPERTY(uint8_t, LV_PERCENT)
+    LV_PROPERTY(uint8_t, CAR_STATUS)
 
-    uint8_t value_LV_CURRENT;
-    lv_obj_t * lb_LV_CURRENT;
-
-    uint8_t value_LV_TEMP;
-    lv_obj_t * lb_LV_TEMP;
-
-    uint8_t value_LV_PERCENT;
-    lv_obj_t * lb_LV_PERCENT;
-
-
-
-    uint8_t value_CAR_STATUS;
-    uint8_t lb_CAR_STATUS;
-
-
-
-/*----secondary messages-----*/
-    uint8_t value_GPS_SPEED;
-    lv_obj_t * lb_GPS_SPEED;
-
-    uint8_t value_LAP_COUNT;
-    lv_obj_t * lb_LAP_COUNT;
-
+    /*----secondary messages-----*/
+    LV_PROPERTY(uint8_t, GPS_SPEED)
+    LV_PROPERTY(uint8_t, LAP_COUNT)
 
     /*secondary_message_CONTROL_OUTPUT;*/
-
-    uint8_t value_ESTIMATED_VELOCITY;
-    lv_obj_t * lb_ESTIMATED_VELOCITY;
-
-    uint8_t value_TORQUE;
-    lv_obj_t * lb_TORQUE;
+    LV_PROPERTY(uint8_t, ESTIMATED_VELOCITY)
+    LV_PROPERTY(uint8_t, TORQUE)
     
     /*temps tyres*/
-
-    uint8_t value_FL_TEMP;
-    lv_obj_t * lb_FL_TEMP;
-
-    uint8_t value_FR_TEMP;
-    lv_obj_t * lb_FR_TEMP;
-
-    uint8_t value_RL_TEMP;
-    lv_obj_t * lb_RL_TEMP;
-
-    uint8_t value_RR_TEMP;
-    lv_obj_t * lb_RR_TEMP;
-
+    LV_PROPERTY(uint8_t, FL_TEMP)
+    LV_PROPERTY(uint8_t, FR_TEMP)
+    LV_PROPERTY(uint8_t, RL_TEMP)
+    LV_PROPERTY(uint8_t, RR_TEMP)
 
     /*pressure tyres*/
-
-    uint8_t value_FL_PRESS;
-    lv_obj_t * lb_FL_PRESS;
-
-    uint8_t value_FR_PRESS;
-    lv_obj_t * lb_FR_PRESS;
-
-    uint8_t value_RL_PRESS;
-    lv_obj_t * lb_RL_PRESS;
-
-    uint8_t value_RR_PRESS;
-    lv_obj_t * lb_RR_PRESS;
-
+    LV_PROPERTY(uint8_t, FL_PRESS)
+    LV_PROPERTY(uint8_t, FR_PRESS)
+    LV_PROPERTY(uint8_t, RL_PRESS)
+    LV_PROPERTY(uint8_t, RR_PRESS)
 
 /*----the following are guessed (don't know which messages are in can)----*/
-
-    uint8_t value_MOTOR_TEMP;
-    lv_obj_t * lb_MOTOR_TEMP;
-
-    uint8_t value_INVERTER_TEMP;
-    lv_obj_t * lb_INVERTER_TEMP;
-
+    LV_PROPERTY(uint8_t, MOTOR_TEMP)
+    LV_PROPERTY(uint8_t, INVERTER_TEMP)
 
     /*values for lap*/
-    uint8_t value_BEST_TIME;
-    lv_obj_t * lb_BEST_TIME;
-
-    uint8_t value_LAST_TIME;
-    lv_obj_t * lb_LAST_TIME;
-
-    uint8_t value_DELTA_TIME;
-    lv_obj_t * lb_DELTA_TIME;
-
-    
-
-    uint8_t value_SLIP;
-    lv_obj_t * lb_SLIP;
-
-    uint8_t value_POWER;
-    lv_obj_t * lb_POWER;
-
-    uint8_t value_KM;
-    lv_obj_t * lb_KM;
-
-    
+    LV_PROPERTY(uint8_t, BEST_TIME)
+    LV_PROPERTY(uint8_t, LAST_TIME)
+    LV_PROPERTY(uint8_t, DELTA_TIME)
+    LV_PROPERTY(uint8_t, SLIP)
+    LV_PROPERTY(uint8_t, POWER)
+    LV_PROPERTY(uint8_t, KM)
 
     /*parts to elements that can't be accessed otherwise*/
-
     lv_obj_t *custom_meter;
-
 
 } steering_t;
 
