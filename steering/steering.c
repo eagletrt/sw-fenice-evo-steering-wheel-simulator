327a6f2bc43d87b419369e8bce481f8064eda850
fedzzzz <fedi.adami@gmail.com>
2023-05-23 22:19:23 +0200

    setting can.sh

--- steering/steering.c
+++ steering/steering.c
@@ -4,9 +4,7 @@ steering_t steering;
 
 void test_value_update_incremental(void){
     LV_UPDATE_LABEL(general_info, estimated_velocity, ++steering.general_info.estimated_velocity);
-    if(steering.general_info.estimated_velocity > steering.control.power){
-
-        
+    if(steering.general_info.estimated_velocity > steering.control.power){   
        steering.general_info.estimated_velocity = 1;
     }
     lv_meter_set_indicator_end_value(steering.custom_meter, steering.indicator_blue, steering.general_info.estimated_velocity);
