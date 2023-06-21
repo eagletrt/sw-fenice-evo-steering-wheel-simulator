3477ffe41baddb8db56f9c2f773d51a104a95e94
tzarjakob <giacomo.mazzucchi@protonmail.com>
2023-06-21 13:36:11 +0200

    BROKEN: renamed the components of the main structure steering_t
    all the reference must change accordingly

--- steering/steering.c
+++ steering/steering.c
@@ -1,4 +1,99 @@
 #include "steering.h"
 
-steering_t steering;
+steering_t steering = {
+    .das.car_status = primary_car_status_car_status_IDLE,
+    .das.inverter_left = primary_car_status_inverter_l_OFF,
+    .das.inverter_right = primary_car_status_inverter_r_OFF,
+    .das.left_speed_rads = 0,
+    .das.right_speed_rads = 0,
+    .das.speed = 0,
+    .das.apps = 0,
+    .das.bse = 0,
+    .das.steering_angle = 90,
+    .das.encoder_right = 0,
+    .das.encoder_left = 0,
+    .das.version_component = 0,
+    .das.version_cancicd = 0,
+    .das.errors = 0,
 
+
+    .hv.pack_voltage = 0,
+    .hv.bus_voltage = 0,
+    .hv.max_cell_voltage = 0,
+    .hv.min_cell_voltage = 0,
+    .hv.current = 0,
+    .hv.power = 0,
+    .hv.average_temperature = 0,
+    .hv.max_temperature = 0,
+    .hv.min_temperature = 0,
+    .hv.errors = 0,
+    .hv.feedbacks = 0,
+    .hv.warnings = 0,
+    .hv.ts_status = primary_ts_status_ts_status_OFF,
+    .hv.version_component = 0,
+    .hv.version_cancicd = 0,
+
+    .inverters.right_temperature = 0.0f,
+    .inverters.left_temperature = 0.0f,
+    .inverters.right_errors = 0,
+    .inverters.right_status = 0,
+    .inverters.right_io_info = 0,
+    .inverters.right_motor_temp = 0.0f,
+    .inverters.right_inverter_temp = 0.0f,
+    .inverters.right_speed = 0,
+    .inverters.left_errors = 0,
+    .inverters.left_status = 0,
+    .inverters.left_io_info = 0,
+    .inverters.left_motor_temp = 0.0f,
+    .inverters.left_inverter_temp = 0.0f,
+    .inverters.left_speed = 0,
+
+    .low_voltage.current = 0,
+    .low_voltage.voltage = 0,
+    .low_voltage.total_voltage = 0,
+    .low_voltage.dcdc_temperature = 0,
+    .low_voltage.battery_temperature = 0,
+    .low_voltage.version_component = 0,
+    .low_voltage.version_cancicd = 0,
+    .low_voltage.errors = 0,
+
+    .steering.temperature = 0,
+    .steering.ptt = false,
+    .steering.canlib_build_hash = 0,
+    .steering.debug_mode = false,
+    .steering.ambient_temperature = 0,
+    .steering.best_time = 0,
+    .steering.last_time = 0,
+    .steering.delta_time = 0,
+    .steering.estimated_velocity = 0,
+
+    .telemetry.latitude = 0.0f,
+    .telemetry.longitude = 0.0f,
+    .telemetry.gps_speed = 0,
+    .telemetry.version_component = 0,
+    .telemetry.version_cancicd = 0,
+    .telemetry.lap_count = 0,
+
+    // check with dynamics team
+    .control.power = 0,
+    .control.torque = 0,
+    .control.slip = 0,
+
+    .tyre_temps.fl_temp = 0,
+    .tyre_temps.fr_temp = 0,
+    .tyre_temps.rl_temp = 0,
+    .tyre_temps.rr_temp = 0,
+    .tyre_pressures.fl_press = 0,
+    .tyre_pressures.fr_press = 0,
+    .tyre_pressures.rl_press = 0,
+    .tyre_pressures.rr_press = 0,
+    
+    .cooling_status.radiators_speed = 0,
+    .cooling_status.pumps_speed = 0,
+
+    .timestamp = 0,
+};
+
+void car_status_update(primary_car_status_t* data) {
+
+}
--- steering/steering.h
+++ steering/steering.h
@@ -2,6 +2,7 @@
 #define STEERING_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include "lvgl.h"
 #include <time.h>
@@ -14,20 +15,15 @@
 
 #define NUM_TABS 5
 
-typedef enum{
+typedef enum {
     BSE,
     STEER,
     APPS
 } CalibrationBox;
 
-/*
-    THE LABELS ARE ASSIGNED TO EACH TAB WITH TabIdentification structure in utils.h,
-     HOPEFULLY THERE WILL BE NO CONFLICT ON THE SAME LABEL
-*/
-
 #define LV_PROPERTY(type, name) \
     type name; \
-    lv_obj_t * lb_##name[NUM_TABS];
+    lv_obj_t* lb_##name[NUM_TABS];
 
 #define LV_UPDATE_LABEL(struct, name, value) \
     steering.struct.name = value; \
@@ -36,58 +32,110 @@ typedef enum{
             lv_label_set_text_fmt(steering.struct.lb_##name[i], "%d", steering.struct.name); \
     }
 
-#define LV_PIPPO(struct, name, value) \
-    steering.struct.name = value; \
-    update_##name(value);
-
-
-
-#define LV_UPDATE_VALUE(struct, name, value) \
-    steering.struct.name = value;
-
-
 typedef struct {
- 
     struct {
-        LV_PROPERTY(uint8_t, hv_voltage)
-        LV_PROPERTY(uint8_t, hv_delta_volt)
-        LV_PROPERTY(uint8_t, hv_current)
-        LV_PROPERTY(uint8_t, hv_temp)
-        LV_PROPERTY(uint8_t, hv_percent)
+        LV_PROPERTY(uint8_t, car_status);
+        LV_PROPERTY(uint8_t, inverter_left);
+        LV_PROPERTY(uint8_t, inverter_right);
+        LV_PROPERTY(int32_t, left_speed_rads);
+        LV_PROPERTY(int32_t, right_speed_rads);
+        LV_PROPERTY(float, speed);
+        LV_PROPERTY(uint16_t, apps);
+        LV_PROPERTY(float, bse);
+        LV_PROPERTY(float, steering_angle);
+        LV_PROPERTY(uint16_t, encoder_right);
+        LV_PROPERTY(uint16_t, encoder_left);
+        LV_PROPERTY(uint8_t, version_component);
+        LV_PROPERTY(uint32_t, version_cancicd);
+        LV_PROPERTY(uint32_t, errors);
+    } das;
+
+    struct {
+        LV_PROPERTY(float, pack_voltage);
+        LV_PROPERTY(uint16_t, bus_voltage);
+        LV_PROPERTY(float, max_cell_voltage);
+        LV_PROPERTY(float, min_cell_voltage);
+        LV_PROPERTY(uint16_t, current);
+        LV_PROPERTY(int16_t, power);
+        LV_PROPERTY(float, average_temperature);
+        LV_PROPERTY(float, max_temperature);
+        LV_PROPERTY(float, min_temperature);
+        LV_PROPERTY(uint16_t, errors);
+        LV_PROPERTY(uint32_t, feedbacks);
+        LV_PROPERTY(uint16_t, warnings);
+        LV_PROPERTY(primary_ts_status_t, ts_status);
+        LV_PROPERTY(uint8_t, version_component);
+        LV_PROPERTY(uint32_t, version_cancicd);
     } hv;
 
     struct {
-        LV_PROPERTY(uint8_t, lv_voltage)
-        LV_PROPERTY(uint8_t, lv_delta_volt)
-        LV_PROPERTY(uint8_t, lv_current)
-        LV_PROPERTY(uint8_t, lv_temp)
-        LV_PROPERTY(uint8_t, lv_percent)
-        LV_PROPERTY(uint8_t, car_status)
+        LV_PROPERTY(float, right_temperature);
+        LV_PROPERTY(float, left_temperature);
+        LV_PROPERTY(uint32_t, right_errors);
+        LV_PROPERTY(uint32_t, right_status);
+        LV_PROPERTY(uint16_t, right_io_info);
+        LV_PROPERTY(float, right_motor_temp);
+        LV_PROPERTY(float, right_inverter_temp);
+        LV_PROPERTY(uint32_t, right_speed);
+        LV_PROPERTY(uint32_t, left_errors);
+        LV_PROPERTY(uint32_t, left_status);
+        LV_PROPERTY(uint16_t, left_io_info);
+        LV_PROPERTY(float, left_motor_temp);
+        LV_PROPERTY(float, left_inverter_temp);
+        LV_PROPERTY(uint32_t, left_speed);
+    } inverters;
+
+    struct {
+        LV_PROPERTY(float, current);
+        LV_PROPERTY(float, voltage);
+        LV_PROPERTY(uint16_t, total_voltage);
+        LV_PROPERTY(float, dcdc_temperature);
+        LV_PROPERTY(float, battery_temperature);
+        LV_PROPERTY(uint8_t, version_component);
+        LV_PROPERTY(uint32_t, version_cancicd);
+        LV_PROPERTY(uint32_t, errors);
     } low_voltage;
 
     struct {
-        LV_PROPERTY(uint8_t, gps_speed)
-        LV_PROPERTY(uint8_t, lap_count)
-    } secondary_messages;
+        LV_PROPERTY(float, temperature);
+        LV_PROPERTY(bool, ptt);
+        LV_PROPERTY(uint32_t, canlib_build_hash);
+        LV_PROPERTY(bool, debug_mode);
+        LV_PROPERTY(uint8_t, ambient_temperature);
+        LV_PROPERTY(uint8_t, best_time);
+        LV_PROPERTY(uint8_t, last_time);
+        LV_PROPERTY(uint8_t, delta_time);
+        LV_PROPERTY(uint8_t, estimated_velocity);
+    } steering;
 
     struct {
-        LV_PROPERTY(uint8_t, power)
-        LV_PROPERTY(uint8_t, torque)
-        LV_PROPERTY(uint8_t, slip)
-    } control;
-    
+        LV_PROPERTY(float, latitude);
+        LV_PROPERTY(float, longitude);
+        LV_PROPERTY(uint16_t, gps_speed);
+        LV_PROPERTY(uint8_t, version_component);
+        LV_PROPERTY(uint32_t, version_cancicd);
+        LV_PROPERTY(uint8_t, lap_count);
+    } telemetry;
+
+
     struct {
-        LV_PROPERTY(uint8_t, fl_temp)
-        LV_PROPERTY(uint8_t, fr_temp)
-        LV_PROPERTY(uint8_t, rl_temp)
-        LV_PROPERTY(uint8_t, rr_temp)
+        LV_PROPERTY(uint8_t, power);
+        LV_PROPERTY(uint8_t, torque);
+        LV_PROPERTY(uint8_t, slip);
+    } control;
+
+    struct {
+        LV_PROPERTY(uint8_t, fl_temp);
+        LV_PROPERTY(uint8_t, fr_temp);
+        LV_PROPERTY(uint8_t, rl_temp);
+        LV_PROPERTY(uint8_t, rr_temp);
     } tyre_temps;
 
-    struct{
-        LV_PROPERTY(uint8_t, fl_press)
-        LV_PROPERTY(uint8_t, fr_press)
-        LV_PROPERTY(uint8_t, rl_press)
-        LV_PROPERTY(uint8_t, rr_press)
+    struct {
+        LV_PROPERTY(uint8_t, fl_press);
+        LV_PROPERTY(uint8_t, fr_press);
+        LV_PROPERTY(uint8_t, rl_press);
+        LV_PROPERTY(uint8_t, rr_press);
     } tyre_pressures;
 
     struct {
@@ -95,35 +143,20 @@ typedef struct {
         LV_PROPERTY(uint8_t, pumps_speed);
     } cooling_status;
 
-    /*----the following are guessed (don't know which messages are in can)----*/
-    struct {
-        LV_PROPERTY(uint8_t, motor_temp)
-        LV_PROPERTY(uint8_t, inverter_temp)
-    } motor_inverter;
-
-    struct {
-        LV_PROPERTY(uint8_t, ambient_temperature)
-        LV_PROPERTY(uint8_t, best_time)
-        LV_PROPERTY(uint8_t, last_time)
-        LV_PROPERTY(uint8_t, delta_time)
-        LV_PROPERTY(uint8_t, estimated_velocity)
-        //uint8_t estimated_velocity;
-        LV_PROPERTY(uint8_t, km)
-    } general_info;
 
     lv_obj_t *custom_meter;
     lv_meter_indicator_t *indicator_blue;
     lv_meter_indicator_t *indicator_white;
-    //lv_obj_t *indicator_blue;
-    //lv_obj_t *indicator_white;
 
-    /* calibration tab*/
     CalibrationBox curr_focus;
-    lv_obj_t *slider; 
+    lv_obj_t *slider;
+
+    uint32_t timestamp;
 
 } steering_t;
 
 
 extern steering_t steering;
+void car_status_update(primary_car_status_t* data);
 
 #endif
